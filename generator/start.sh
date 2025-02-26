399d8d2cd89d02be0ae97f27a4e13acf0216be39
lalyos <lalyos@yahoo.com>
2025-02-26 11:01:14 +0100

    compose build

--- generator/start.sh
+++ generator/start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-echo "<h1>Tuesday Menu v2<h1>" > /www/index.html
-sleep 5
-## todo wait for db connection
+while true; do
+  echo "<h1>Tuesday Menu v2<h1>" > /www/index.html
 
-mariadb \
-  mysql \
-  --html \
-  --password=s3cr3t  \
-  --execute='select * from food' \
-  --host=db  \
-  >> /www/index.html
+  mariadb \
+    mysql \
+    --html \
+    --password=s3cr3t  \
+    --execute='select * from food' \
+    --host=db  \
+    >> /www/index.html
+
+  sleep ${SLEEP:-5}
+done
